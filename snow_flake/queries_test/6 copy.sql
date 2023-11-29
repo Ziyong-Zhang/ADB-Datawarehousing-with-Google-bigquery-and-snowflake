81eeddc04ba722de42f018743f0aa9b603b85a1a
Ziyong-Zhang <147911303+Ziyong-Zhang@users.noreply.github.com>
2023-11-29 20:58:35 +0100

    Add files via upload

---
+++ snow_flake/queries_test/6 copy.sql
@@ -0,0 +1,15 @@
+-- $ID$
+-- TPC-H/TPC-R Forecasting Revenue Change Query (Q6)
+-- Functional Query Definition
+-- Approved February 1998
+
+select
+	sum(l_extendedprice * l_discount) as revenue
+from
+	lineitem
+where
+    l_shipdate >= date '1997-11-11'
+    and l_shipdate < dateadd(YEAR, 1, DATE'1997-11-11')
+    and l_discount between 0.07 - 0.01 and 0.07 + 0.01
+    and l_quantity < 50;
+
