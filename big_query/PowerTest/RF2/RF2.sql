0212624e460aa6056935d8a3a81d0ee3458d4c4b
zhuangxianyun <149632845+zhuangxianyun@users.noreply.github.com>
2023-12-05 20:08:37 +0100

    Update RF2.sql

--- big_query/PowerTest/RF2/RF2.sql
+++ big_query/PowerTest/RF2/RF2.sql
@@ -1,1 +1,2 @@
-
+	delete from `advanceddb-405622.TPCH_3G.LINEITEM` where L_ORDERKEY in (select O_ORDERKEY from `advanceddb-405622.TPCH_3G.delete_orders`);
+	delete from `advanceddb-405622.TPCH_3G.ORDERS` where O_ORDERKEY in (select O_ORDERKEY from `advanceddb-405622.TPCH_3G.delete_orders`);
