65edc25a04aea1e3aaa3886b545fc8d7158abf0e
zhuangxianyun <149632845+zhuangxianyun@users.noreply.github.com>
2023-12-05 20:07:08 +0100

    Update RF1.sql

--- big_query/PowerTest/RF1/RF1.sql
+++ big_query/PowerTest/RF1/RF1.sql
@@ -1,1 +1,9 @@
-sql文
+--insert data
+
+INSERT INTO `advanceddb-405622.TPCH_3G.ORDERS`
+SELECT * FROM `advanceddb-405622.TPCH_3G.add_orders` ;
+
+INSERT INTO `advanceddb-405622.TPCH_3G.LINEITEM`
+SELECT * FROM `advanceddb-405622.TPCH_3G.add_lineitem`;
+
+
